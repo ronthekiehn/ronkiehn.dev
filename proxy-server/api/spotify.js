f843103ad8fd6329f207786d07211d358fb4816c
Ron Kiehn <ronthekiehn@gmail.com>
2024-08-21 23:06:09 -0500

    logs

--- proxy-server/api/spotify.js
+++ proxy-server/api/spotify.js
@@ -26,8 +26,9 @@ const getAccessToken = async () => {
         if (!response.ok) {
             throw new Error(`Failed to get access token: ${response.statusText}`);
         }
-
-        return response.json();
+        console.log(response);
+        console.log(response.json());
+        return await response.json();
     } catch (error) {
         console.error("Error in getAccessToken:", error);
         throw error;
@@ -77,55 +78,3 @@ export default async function handler(req, res) {
         res.status(500).json({ error: 'Failed to fetch now playing data' });
     }
 }
-
-
-// const getAccessToken = async () => {
-//   const authString = Buffer.from(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`).toString('base64');
-//   console.log("TOKENS:", process.env.SPOTIFY_CLIENT_ID, process.env.SPOTIFY_CLIENT_SECRET, process.env.SPOTIFY_REFRESH_TOKEN);
-//   const response = await fetch(
-//     TOKEN_ENDPOINT,
-//     new URLSearchParams({
-//       grant_type: 'refresh_token',
-//       refresh_token: process.env.SPOTIFY_REFRESH_TOKEN,
-//     }),
-//     {
-//       headers: {
-//         Authorization: `Basic ${authString}`,
-//         'Content-Type': 'application/x-www-form-urlencoded',
-//       },
-//     }
-//   );
-//   console.log("RESPONSE:", response);
-//   return response.json;
-// };
-
-// export default async function handler(req, res) {
-//   try {
-//     const accessToken = await getAccessToken();
-
-//     const nowPlayingResponse = await fetch(NOW_PLAYING_ENDPOINT, {
-//       headers: {
-//         Authorization: `Bearer ${accessToken}`,
-//       },
-//     });
-
-//     if (nowPlayingResponse.status === 204 || nowPlayingResponse.status > 400) {
-//       return res.json({ isPlaying: false });
-//     }
-
-//     const song = nowPlayingResponse.data;
-
-//     const nowPlayingData = {
-//       albumImageUrl: song.item.album.images[0].url,
-//       artist: song.item.artists.map((artist) => artist.name).join(', '),
-//       isPlaying: song.is_playing,
-//       songUrl: song.item.external_urls.spotify,
-//       title: song.item.name,
-//     };
-
-//     res.json(nowPlayingData);
-//   } catch (error) {
-//     console.error('Error fetching now playing data:', error);
-//     res.status(500).json({ error: 'Failed to fetch now playing data' });
-//   }
-// };
