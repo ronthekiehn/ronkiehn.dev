d77bad8a626fda1aeccb506d572736f882814b17
Ron Kiehn <ronthekiehn@gmail.com>
2024-08-23 18:44:47 -0500

    mobile 2

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import { useEffect } from 'react';
 import '../styles/AboutMe.css';
 
 const name = ['r', 'o', 'n'];
 let alt = false;
+
+
 const AboutMe = () => {
+    useEffect(() => {
+        const container = document.querySelector('.box-container');
+        
+        const handleTouchMove = (e) => {
+          e.preventDefault(); // Prevent scrolling
+          const touch = e.touches[0];
+          const element = document.elementFromPoint(touch.clientX, touch.clientY);
+          if (element && element.classList.contains('box')) {
+            element.classList.add('hovered');
+            setTimeout(() => element.classList.remove('hovered'), 500); // Remove class after 500ms
+          }
+        };
+    
+        const handleTouchEnd = () => {
+          const hoveredSquares = document.querySelectorAll('.box.hovered');
+          hoveredSquares.forEach(square => square.classList.remove('hovered'));
+        };
+    
+        container.addEventListener('touchmove', handleTouchMove, { passive: true });
+        container.addEventListener('touchend', handleTouchEnd,  { passive: true });
+    
+        return () => {
+          container.removeEventListener('touchmove', handleTouchMove);
+          container.removeEventListener('touchend', handleTouchEnd);
+        };
+      }, []);
 return (
     <div className='container'>
             <div className="box-container">
