203c261611d9491312bb77b9729d5a958abf54be
Ron Kiehn <ronthekiehn@gmail.com>
2024-11-30 17:19:35 -0600

    set up for blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), alpinejs()],
   site: 'https://ronkiehn.dev',
 });
---
+++ src/posts/twitter-aura.md
@@ -0,0 +1,21 @@
+---
+title: "What I Learned From Making A Viral Website"
+author: "Ron Kiehn"
+date: "2024-11-30"
+---
+
+
+
+## 1. Copy People
+
+## 2. Svelte > React
+
+## 3. Don't Pay For Things You Can Make Yourself
+
+## 4. Serverless is Great, Actually
+
+## 5. Open Source is King
+
+## 6. Be Careful
+
+## 7. Make Money
