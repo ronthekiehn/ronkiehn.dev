17f2c96092c3911290be3568ee15b8120e85f3f5
Your Name <51499585+ronthekiehn@users.noreply.github.com>
2025-05-06 15:46:53 -0500

    setting up other page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,10 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
-import alpinejs from '@astrojs/alpinejs';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), alpinejs()],
+  integrations: [tailwind(), react(), mdx()],
   site: 'https://ronkiehn.dev',
 });
